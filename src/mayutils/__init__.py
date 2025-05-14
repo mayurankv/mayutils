607ae5dcaec3254abe29da183d2eea554fcf1e5d
Mayuran Visakan <mayuran.visakan@lendable.co.uk>
2025-05-14 14:18:31 +0100

    Bump version to 1.0.16

--- src/mayutils/__init__.py
+++ src/mayutils/__init__.py
@@ -1,4 +1,4 @@
-from mayutils.environment.logging import Logging
+from mayutils.environment.logging import Logger
 from mayutils.objects.dataframes import (
     setup_dataframes,
 )
@@ -6,11 +6,11 @@ from mayutils.visualisation.notebook import setup_notebooks
 
 
 def setup() -> None:
-    Logging.configure()
+    Logger.configure()
     setup_notebooks()
     setup_dataframes()
 
 
 setup()
 
-__version__ = "1.0.15"
+__version__ = "1.0.16"
