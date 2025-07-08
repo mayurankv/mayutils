d455dd0ca5ebc8827532918a0a429907893fb36e
Mayuran Visakan <69591376+mayurankv@users.noreply.github.com>
2025-07-08 10:34:07 +0100

    update

--- src/mayutils/__init__.py
+++ src/mayutils/__init__.py
@@ -1,3 +1,4 @@
+import warnings
 from mayutils.environment.logging import Logger
 from mayutils.objects.dataframes import (
     setup_dataframes,
@@ -9,6 +10,14 @@ def setup() -> None:
     Logger.configure()
     setup_notebooks()
     setup_dataframes()
+
+    # TODO: Remove when dependency is upgraded
+    warnings.filterwarnings(
+        action="ignore",
+        message="You have an incompatible version of 'pyarrow' installed.*",
+        category=UserWarning,
+        module="snowflake.connector.options",
+    )
 
 
 setup()
