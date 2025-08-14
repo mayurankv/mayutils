f0eecd5d431e05753d5437f56b47fa382b47c022
Mayuran Visakan <69591376+mayurankv@users.noreply.github.com>
2025-08-14 09:38:45 +0100

    update

--- src/mayutils/visualisation/console.py
+++ src/mayutils/visualisation/console.py
@@ -1,12 +1,13 @@
 import builtins
 from contextlib import contextmanager
-from typing import Any, Generator
+from typing import Any, Callable, Generator, Optional
 from unicodeit import replace
 from rich import pretty, traceback
 from rich import print as rprint
 
 
 PRINT = builtins.print
+
 
 def console_latex(
     latex: str,
@@ -16,10 +17,13 @@ def console_latex(
 
 @contextmanager
 def replace_print(
-    print_method,
+    print_method: Optional[Callable] = None,
 ) -> Generator[None, Any, None]:
+    base_print = (
+        __builtins__["print"] if isinstance(__builtins__, dict) else __builtins__.print
+    )
     original = builtins.print
-    builtins.print = print_method
+    builtins.print = print_method if print_method is not None else base_print
     try:
         yield
     finally:
